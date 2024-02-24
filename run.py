89335672c32b14270a70b6e52143b3da12cdb19b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2024-02-24 20:48:34 +0100

    terminado

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
 import os
-from scrapping import scrapeo
-
 os.system("pip install -r requirements.txt")
+# El run.py en esta entrega no es necesario, pero se incluye para que el usuario pueda instalar las dependencias necesarias para correr el proyecto ya que todo está en jupyter notebook.
